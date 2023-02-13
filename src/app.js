476e1112700bbfcb14f1c6303351f819c44814aa
Zirak Waheed <zirak.waheed@square63.com>
2023-02-13 17:07:09 +0500

    Added bootstrap

--- src/app.js
+++ src/app.js
@@ -13,13 +13,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
+app.use(express.static(path.join(__dirname, '../node_modules/bootstrap/dist/')));
 app.use(express.static(path.join(__dirname, '../public')));
 hbs.registerPartials(path.join(__dirname, '../templates/partials'))
 app.use(sessions({
   secret: process.env.JWT_SECRET,
   saveUninitialized:true,
   cookie: { maxAge: 86400000 },
-  resave: false 
+  resave: false
 }));
 app.use(cookieParser());
 app.use(userRouter)
